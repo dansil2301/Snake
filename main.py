544ed71dad813c2ef8488b35f5a986dc0384557b
Danila Solovenko <dyusolovenko@edu.hse.ru>
2022-07-13 22:06:06 +0300

    fix

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from random import randrange
 
 
 def main(stdscr):
+    curses.resize_term(130, 60)
+    #stdscr.refresh()
+
     # Цвета необходимые для игры
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
